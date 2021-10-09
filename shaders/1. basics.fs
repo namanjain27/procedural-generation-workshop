de65b34231be225d8f5b0df64be7b8839d03781d
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-10-09 14:47:52 +0530

    Update comments 1

--- shaders/1. basics.fs
+++ shaders/1. basics.fs
@@ -4,10 +4,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec2 uv = fragCoord / iResolution.xy;
     // Between -1 to 1
     uv= uv * 2.0f - 1.0f;
-    // Between 0 to 1
+    // Between 0 to 1 and mirrored
     uv = abs(uv);
+
     // Outpur Color
     vec3 col = vec3(uv.x, uv.y, (sin(iTime)+1.0f)/2.0f);
+    
     // Output to screen
     fragColor = vec4(col, 1.0);
 }
